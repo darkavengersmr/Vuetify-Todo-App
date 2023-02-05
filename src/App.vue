2520693c6752dfecb3e3c829ce2eea49b1d17169
darkavengersmr <mailsea@yandex.ru>
2023-02-05 21:08:08 +0400

    small fixes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <v-app :theme="store.state.theme">        
-      <router-view />    
+  <v-app :theme="store.state.theme" @click="closeMenu">
+    <router-view />
   </v-app>
 </template>
 
 <script setup>
 import { useStore } from "vuex";
 const store = useStore()
+const closeMenu = () => { if (store.state.navigationMenuActive) store.commit('setNavigationMenuActive', false) }
 </script>
 
 <style>
